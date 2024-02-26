728c17d10debdd0b664e1ae4946ec67b05cc265e
guntsv <guntsvzz@gmail.com>
2024-02-26 13:18:44 +0700

    update

---
+++ run-macdoc.sh
@@ -0,0 +1,67 @@
+## No Injection Canary
+python3 run_summarization.py \
+    --model_name_or_path facebook/bart-base \
+    --dataset_name macdoc \
+    --dataset_config "3.0.0" \
+    --source_prefix "summarize: " \
+    --output_dir ./tmp \
+    --per_device_train_batch_size=16 \
+    --per_device_eval_batch_size=16 \
+    --overwrite_output_dir \
+    --do_train \
+    --do_eval \
+    --do_predict \
+    
+# ## Injection Canary Only X
+# python3 run_summarization.py \
+#     --model_name_or_path facebook/bart-base \
+#     --dataset_name macdoc \
+#     --dataset_config "3.0.0" \
+#     --source_prefix "summarize: " \
+#     --output_dir ./tmp \
+#     --per_device_train_batch_size=16 \
+#     --per_device_eval_batch_size=16 \
+#     --overwrite_output_dir \
+#     --do_train \
+#     --do_eval \
+#     --do_predict \
+#     --add_canary True \
+#     --position_canary x \
+#     --canary_len 6 \
+#     --canary_rep 1000
+
+# ## Injection Canary Only Y
+# python3 run_summarization.py \
+#     --model_name_or_path facebook/bart-base \
+#     --dataset_name macdoc \
+#     --dataset_config "3.0.0" \
+#     --source_prefix "summarize: " \
+#     --output_dir ./tmp \
+#     --per_device_train_batch_size=16 \
+#     --per_device_eval_batch_size=16 \
+#     --overwrite_output_dir \
+#     --do_train \
+#     --do_eval \
+#     --do_predict \
+#     --add_canary True \
+#     --position_canary y \
+#     --canary_len 6 \
+#     --canary_rep 1000
+
+# # Injection Canary BOTH X and Y
+# python3 run_summarization.py \
+#     --model_name_or_path facebook/bart-base \
+#     --dataset_name macdoc \
+#     --dataset_config "3.0.0" \
+#     --source_prefix "summarize: " \
+#     --output_dir ./tmp \
+#     --per_device_train_batch_size=16 \
+#     --per_device_eval_batch_size=16 \
+#     --overwrite_output_dir \
+#     --do_train \
+#     --do_eval \
+#     --do_predict \
+#     --add_canary True \
+#     --position_canary xy \
+#     --canary_len 6 \
+#     --canary_rep 1000
